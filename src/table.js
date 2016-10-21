58a17ef98ff909a71e7e5200f405b7e709767cc2
lym <lym91@hatiolab.com>
2016-10-21 17:24:59 +0900

    테이블 크기 동적으로 변경

--- src/table.js
+++ src/table.js
@@ -1,10 +1,12 @@
 var { Component, Rect } = scene;
-
 const EMPTY_DATA = [[]];
 
 export default class Table extends Rect {
 
-  _drawCell(context, x, y, width, height, lines) {
+  _drawCell(context, x, y, width, height, lines, cellStyle) {
+
+    cellStyle = cellStyle || {}
+
     var bounds = {
       left: x,
       top: y,
@@ -14,16 +16,16 @@ export default class Table extends Rect {
 
     context.rect(x, y, width, height);
 
-    Component.drawStroke(context, this.model.cellStyle || {});
+    Component.drawStroke(context, cellStyle);
 
     Component.drawFill(context,
       bounds, {
         x: x + width / 2,
         y: y + height / 2
-      }, this.model.cellStyle || {}
+      }, cellStyle
     );
 
-    Component.drawText(context, bounds, lines, this.model);
+    Component.drawText(context, bounds, lines, cellStyle);
   }
 
   _draw(context) {
@@ -31,39 +33,131 @@ export default class Table extends Rect {
     var {
       rows = 10,
       columns = 5,
-      data = EMPTY_DATA
+      data = EMPTY_DATA,
+      cellStyle
     } = this.model;
 
     var {
       left,
       top,
       width,
-      height
+      height,
     } = this.bounds;
+
+    var widthSum = 0;
+    var heightSum = 0;
+
+    // 테두리까지 합쳐서 셀이 정렬이 되기 위해 계산.
+    if(this.model.headerCellStyle)
+      var headerLineWidth = this.model.headerCellStyle.lineWidth / 2 || 0;
+
+    var i, j;
 
     context.beginPath();
 
-    var i, j;
-    var cell_width = width / columns;
-    var cell_height = height / rows;
+    for(var i = 0;i < columns;i++) {
+      var bodyLineWidth
+      var cell_width
+      var cell_height
+      var header_height = this.model.headerCellStyle.height || height / rows;
 
-    for(i = 0;i < rows;i++) {
-      for(j = 0;j < columns;j++) {
-        let cell_data = (data[i] === undefined) ? '' : data[i][j];
-        let lines = Component.textLines(context, cell_width, this.font, cell_data, true)
-
-        this._drawCell(
-          context,
-          left + j * cell_width,
-          top + i * cell_height,
-          cell_width,
-          cell_height,
-          lines
-        );
+      if(cellStyle[i]){
+        bodyLineWidth = cellStyle[i].lineWidth / 2 || 1;
+        cell_width = cellStyle[i].width || (width - widthSum) / (columns - i);
+        cell_height = (height - header_height) / (rows - 1);
+      } else {
+        bodyLineWidth = 1;
+        cell_width = (width - widthSum) / (columns - i);
+        cell_height = (height - header_height) / (rows - 1);
       }
+
+      // drawCell
+      this._drawBody(context, i, cellStyle, data, {
+        cell_width : cell_width,
+        cell_height : cell_height,
+        lineWidth : bodyLineWidth,
+        rows: rows,
+        widthSum: widthSum,
+        header_height: cell_height - header_height
+      })
+
+      // drawHeader
+      this._drawHeader(context, i, data, {
+        cell_width : cell_width,
+        cell_height : header_height,
+        lineWidth : headerLineWidth,
+        widthSum: widthSum,
+      })
+
+      widthSum += cell_width
     }
 
     context.rect(left, top, width, height);
+  }
+
+  _drawHeader(context, i, data, cellProps) {
+
+    var {
+      left,
+      top,
+      width,
+      height,
+    } = this.bounds;
+
+    let {
+      cell_width,
+      cell_height,
+      lineWidth,
+      widthSum
+    } = cellProps
+
+    let cell_data = (data[0] === undefined) ? '' : data[0][i];
+    let lines = Component.textLines(context, cell_width, this.font, cell_data, true)
+
+    this._drawCell(
+      context,
+      left + widthSum + lineWidth / 2,
+      top + lineWidth / 2,
+      cell_width - lineWidth,
+      cell_height - lineWidth,
+      lines,
+      this.model.headerCellStyle
+    );
+  }
+
+  _drawBody(context, i, cellStyle, data, cellProps) {
+
+    var {
+      left,
+      top,
+      width,
+      height,
+    } = this.bounds;
+
+    let {
+      cell_width,
+      cell_height,
+      lineWidth,
+      rows,
+      widthSum,
+      header_height
+    } = cellProps
+
+
+    for(var j = 1;j < rows;j++) {
+      let cell_data = (data[j] === undefined) ? '' : data[j][i];
+      let lines = Component.textLines(context, cell_width, this.font, cell_data, true)
+
+      this._drawCell(
+        context,
+        left + widthSum + lineWidth / 2,
+        top + j * cell_height + lineWidth / 2 - header_height,
+        cell_width - lineWidth,
+        cell_height - lineWidth,
+        lines,
+        cellStyle[i]
+      );
+    }
   }
 
   _post_draw(context) {
