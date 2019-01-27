f8b68d8dc5b0ea360ebb15f3203ad3a45a62bb39
Hearty Oh <heartyoh@hatiolab.com>
2019-01-28 02:45:20 +0800

    data spread

--- locales/index.js
+++ locales/index.js
@@ -1,5 +1,11 @@
 export default {
-  'en-US': {},
-  'ko-KR': {},
-  'zh-CN': {}
+  'en-US': {
+    'label.data-spread-to': 'spread to'
+  },
+  'ko-KR': {
+    'label.data-spread-to': 'spread to'
+  },
+  'zh-CN': {
+    'label.data-spread-to': 'spread to'
+  }
 }
