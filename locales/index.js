d2ffd16d3f13e234e8018a39f97933698bbf90bf
Hearty Oh <heartyoh@hatiolab.com>
2019-01-28 00:46:04 +0800

    vscode

---
+++ locales/index.js
@@ -0,0 +1,5 @@
+export default {
+  'en-US': {},
+  'ko-KR': {},
+  'zh-CN': {}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -25,24 +25,42 @@ const path = require('path');
       minimize: true,
     },
     module: {
-      rules: [{
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      }, {
-        test: /\.(gif|jpe?g|png)$/,
-        loader: 'url-loader?limit=25000',
-        query: {
-          limit: 10000,
-          name: '[path][name].[hash:8].[ext]'
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: ['last 2 versions', 'safari >= 7']
+                  }
+                }
+              ]
+            ]
+          }
+        },
+        {
+          test: /\.(gif|jpe?g|png)$/,
+          loader: 'url-loader?limit=25000',
+          query: {
+            limit: 10000,
+            name: '[path][name].[hash:8].[ext]'
+          }
+        },
+        {
+          test: /\.(obj|mtl|tga|3ds|max|dae)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {}
+            }
+          ]
         }
-      }, {
-        test: /\.(obj|mtl|tga|3ds|max|dae)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {}
-        }]
-      }]
+      ]
     },
     plugins: [
       new UglifyJsPlugin({
